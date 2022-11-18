280568f5c5e77372581147eaac692204b1ae7109
Jerson <solorsanolopez@gmail.com>
2022-11-17 21:49:07 -0800

    💅 [Fetching] Data with server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,39 @@
-export default function Page() {
+// fectching data nextjs 13
+
+interface Time {
+  unixtime: number
+}
+
+async function getCurrentTime() {
+  // similar to getServersideProps
+  const res = await fetch(
+    'http://worldtimeapi.org/api/timezone/America/Chicago',
+    // {
+    //   cache: 'no-cache',
+    //   headers: {
+    //     'x-foo': 'bar',
+    //   },
+    // } or
+
+    // this will revalidate the data in this timelapse
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  )
+
+  const time: Promise<Time> = await res.json()
+  return time
+}
+
+export default async function SSRPage() {
+  const time = await getCurrentTime()
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Time in chicago</h1>
+      <p>{new Date(time.unixtime * 1000).toString()}</p>
     </div>
   )
 }
